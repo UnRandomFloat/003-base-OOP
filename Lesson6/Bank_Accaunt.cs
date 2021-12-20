fefa60e462f5a10932039067043d0a357aae8dff
RandomFloat <manzhurovam@mail.ru>
2021-12-20 20:52:24 +0500

    Практические задания к уроку №6. Креплю ссылку на пулреквест. Окончательная рализация будет следующим коммитом, через пару часов

---
+++ Lesson6/Bank_Accaunt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnRandomFloat._003_base_OOP.HoweWork
+{
+    class Bank_Accaunt : BankAccPutWith
+    {
+        
+    }
+}
---
+++ Lesson6/_HomeWork6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnRandomFloat._003_base_OOP.HoweWork
+{
+    class _HomeWork6 : MenuAndChoice
+    {
+        private static Bank_Accaunt B_acc1 = new Bank_Accaunt();
+        private static Bank_Accaunt B_acc2 = new Bank_Accaunt();
+        private static Bank_Accaunt B_acc3 = new Bank_Accaunt();
+        Bank_Accaunt[] Bank_accaunts = {B_acc1, B_acc2, B_acc3};
+        
+    }
+}
